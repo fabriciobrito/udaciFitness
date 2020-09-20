f5e36845646633ad010a5d1b892ecbd636aad9f4
Fabricio Brito <fabricio.brito@gmail.com>
2020-09-20 11:28:13 -0300

    fix: AsyncStorage usage, since it was deprecated

--- utils/api.js
+++ utils/api.js
@@ -1,19 +1,18 @@
-import { useAsyncStorage } from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-community/async-storage';
 import { CALENDAR_STORAGE_KEY } from './_calendar';
 
 export function submitEntry({ entry, key }) {
-  return useAsyncStorage(CALENDAR_STORAGE_KEY).mergeItem({
-    [key]: JSON.stringify(entry)
-  })
+  return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY,JSON.stringify({
+      [key]: entry
+    }))
 }
 
 export function removeEntry(key) {
-  return useAsyncStorage(CALENDAR_STORAGE_KEY)
-    .getItem()
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
     .then((results) => {
       const data = JSON.parse(results)
       data[key] = undefined;
       delete data[key];
-      useAsyncStorage(CALENDAR_STORAGE_KEY).setItem(JSON.stringify(data));
+      AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
   })
 }
